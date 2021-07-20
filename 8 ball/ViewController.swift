337e903a03057f3818d8c61593293a7351e5c50a
jonas <filerjo@quincy.edu>
2021-07-20 09:59:47 -0500

    8 ball
    8 ball

--- 8 ball/ViewController.swift
+++ 8 ball/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+var ballArray = ["1", "2", "3", "4", "5"]
+    
+    var randomBallNumber = 0
+   
+@IBOutlet weak var askimageview: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func askwatupressed(_ sender: Any) {
+        newBallImage()
+    }
+    
+    func newBallImage() {
+        randomBallNumber = Int.random(in:0...4)
+        
+        askimageview.image = UIImage(named: ballArray[randomBallNumber])
+        
+    }
 }
 
